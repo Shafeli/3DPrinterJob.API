3829dcc3be4b2be496b1d4117620d5f1677ba30a
shafeli <silashafeli@gmail.com>
2025-09-11 11:46:18 -0700

    Add Entity Framework Core DbContext and SQL connection
    Introduced ApplicationDbContext with DbSets for PrinterJob, Status, and Requester entities. Registered the DbContext in Program.cs using a SQL Server connection string, and added a placeholder for the connection string in appsettings.json to enable database integration.

---
+++ 3DPrinterJob.API/Data/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using _3DPrinterJob.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _3DPrinterJob.API.Data.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<PrinterJob> PrinterJobs { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Requester> Requesters { get; set; }
+    }
+}
--- 3DPrinterJob.API/Program.cs
+++ 3DPrinterJob.API/Program.cs
@@ -1,8 +1,12 @@
+using _3DPrinterJob.API.Data.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
