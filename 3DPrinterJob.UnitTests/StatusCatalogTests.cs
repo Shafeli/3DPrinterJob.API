e9ff924c5b2165c355da26d84a1017dcc58d1d80
shafeli <silashafeli@gmail.com>
2025-09-16 22:06:43 -0700

    Add StatusCatalogTests and update test dependencies
    Added StatusCatalogTests to verify seeded status catalog matches StatType enum values. Removed placeholder UnitTest1.cs. Added Microsoft.EntityFrameworkCore.InMemory package to support in-memory database testing.

---
+++ 3DPrinterJob.UnitTests/StatusCatalogTests.cs
@@ -0,0 +1,35 @@
+﻿using _3DPrinterJob.API.Data.DatabaseContext;
+using _3DPrinterJob.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace _3DPrinterJob.UnitTests
+{
+
+    public class StatusCatalogTests
+    {
+        [Fact]
+        public void SeededCatalog_ShouldMatchEnumValues()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestCatalogDb")
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+            context.Database.EnsureCreated(); // trigger OnModelCreating
+
+            var dbStatuses = context.Statuses
+                .Select(s => s.Stat.ToString())
+                .ToHashSet();
+
+
+            var enumStatuses = Enum.GetNames(typeof(StatType));
+
+            Assert.Equal(enumStatuses.Length, dbStatuses.Count);
+            foreach (var status in enumStatuses)
+            {
+                Assert.Contains(status, dbStatuses);
+            }
+        }
+    }
+}
