f99d80c525741ffb1751109b72ee1e10859a3dc0
Shafeli <59191017+Shafeli@users.noreply.github.com>
2025-09-26 11:20:22 -0700

    Create RequesterCatalogTests.cs
    RequesterCatalogTests Started
    first test adding values to requesters

---
+++ 3DPrinterJob.UnitTests/RequesterCatalogTests.cs
@@ -0,0 +1,48 @@
+﻿using _3DPrinterJob.API.Data.DatabaseContext;
+using _3DPrinterJob.UnitTests.TestHelpers;
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _3DPrinterJob.UnitTests
+{
+    public class RequesterCatalogTests
+    {
+
+        // To run this test dotnet test --filter "Name=SeededCatalog_ShouldNotContainDuplicates"
+        [Fact]
+        public void SeededCatalog_ShouldNotContainDuplicates()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("TestCatalogDb_NoDuplicates_Requester")
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+            context.Database.EnsureCreated();
+
+            // need to seed some Requesters for testing
+            DataHelper.SeedRequester(context, "Bob");
+            DataHelper.SeedRequester(context, "Alice");
+            DataHelper.SeedRequester(context, "Robert");
+
+
+            // context.SaveChanges();
+
+            var duplicates = context.Requesters
+                .ToList()
+                .GroupBy(r => r.Name)
+                .Where(g => g.Count() > 1)
+                .ToList();
+
+            Assert.Empty(duplicates);
+        }
+
+        // Additional tests for Requester catalog can be added here.
+
+    }
+}
